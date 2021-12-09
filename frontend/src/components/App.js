f64a245e8fb947ab31d7a8ba80b97bc4179e1955
Shaun Befort <shaun.befort@gomolearning.com>
2021-12-09 00:54:49 -0600

    Added dark and light theme capabilities

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,12 +1,18 @@
+import { ThemeProvider } from 'styled-components';
+import { lightTheme, darkTheme } from '../styles/theme';
+import { GlobalStyles } from '../styles/global';
+
+const params = new URLSearchParams(window.location.search);
+const theme = params.get('theme') === 'dark' ? darkTheme : lightTheme;
+
 function App() {
   return (
-    <div className="App">
-      <header>
-        <p>
-          Science 37 React Code Challenge
-        </p>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <h1>Science 37 React Code Challenge</h1>
+      <footer>
+      </footer>
+    </ThemeProvider>
   );
 }
 
