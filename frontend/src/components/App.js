489a44684640d2af79beca6724031d914e0280d4
Shaun Befort <shaun.befort@gomolearning.com>
2021-12-11 14:30:54 -0600

    Start of main app layout

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,17 +1,39 @@
+import { useState } from 'react';
 import { ThemeProvider } from 'styled-components';
 import { lightTheme, darkTheme } from '../styles/theme';
 import { GlobalStyles } from '../styles/global';
+import SplitScreen from './SplitScreen';
+import Section from './Section';
+import H1 from './H1';
+import FormInput from './FormInput';
+import useTwitterProxy from '../hooks/useTwitterProxy';
 
 const params = new URLSearchParams(window.location.search);
 const theme = params.get('theme') === 'dark' ? darkTheme : lightTheme;
 
 function App() {
+  const data = useTwitterProxy('/api/v1/proxy');
+  const [searchTerm, setSearchTerm] = useState('');
+  console.log(data);
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <h1>Science 37 React Code Challenge</h1>
-      <footer>
-      </footer>
+      <div id="root-container">
+        <Section marginTop={20} marginBottom={12}>
+          <H1>Tweet Feed</H1>
+        </Section>
+        <SplitScreen leftWeight={7} rightWeight={3}>
+          <Section>
+            <FormInput onChange={(e) => setSearchTerm(e.target.value)} placeholder="Search by keyword" />
+            <button onClick={() => console.log(searchTerm)}>Click Me</button>
+          </Section>
+          <Section>
+            hashtags go here
+          </Section>
+        </SplitScreen>
+        <footer>
+        </footer>
+      </div>
     </ThemeProvider>
   );
 }
