d23f6681c2a03aff4cb070a3d249f518b54daa18
Shaun Befort <shaun.befort@gomolearning.com>
2021-12-11 14:30:34 -0600

    Update global styles and themes

--- frontend/src/styles/global.js
+++ frontend/src/styles/global.js
@@ -1,9 +1,31 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+  }
+
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+
   body {
     background: ${({ theme }) => theme.body};
     color: ${({ theme }) => theme.text};
     transition: all 0.25s linear;
+    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+    font-size: 1rem;
+  }
+
+  #root-container {
+    max-width: 1200px;
+    margin: auto;
+    margin-top: 20px;
+  }
+
+  @media (max-width: 800px) {
+    body {
+      font-size: 0.9rem;
+    }
   }
 `;
--- frontend/src/styles/theme.js
+++ frontend/src/styles/theme.js
@@ -1,13 +1,9 @@
 export const lightTheme = {
-  body: '#E2E2E2',
-  text: '#363537',
-  toggleBorder: '#FFF',
-  gradient: 'linear-gradient(#39598A, #79D7ED)',
+  body: '#f8f9f9',
+  text: '#334750',
 }
   
 export const darkTheme = {
   body: '#363537',
-  text: '#FAFAFA',
-  toggleBorder: '#6B8096',
-  gradient: 'linear-gradient(#091236, #1E215D)',
+  text: '#fafafa',
 }
