2ca254414765733e5e3244697be9e12c6dcb2c38
Shaun Befort <shaun.befort@gomolearning.com>
2021-12-12 16:18:27 -0600

    Debounce requests

--- frontend/src/hooks/useTwitterProxy.js
+++ frontend/src/hooks/useTwitterProxy.js
@@ -1,14 +1,19 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import debounce from 'lodash/debounce';
+
+const debounced = debounce(axios.get, 250);
 
 const useTwitterProxy = (searchTerm) => {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
+    if (!searchTerm) return [];
     (async () => {
-      const response = await axios.get(`/api/v1/proxy?q=${searchTerm}`);
-      console.log('responsoe', response);
-      setData(response.data);
+      const response = await debounced(`/api/v1/proxy?q=${encodeURIComponent(searchTerm)}`);
+      if (response) {
+        setData(response.data);
+      }
     })();
   }, [searchTerm]);
 
