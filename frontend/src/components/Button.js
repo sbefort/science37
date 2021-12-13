a4b51257cebdd362d235d51f295f994760511871
Shaun Befort <shaun.befort@gomolearning.com>
2021-12-13 06:00:56 -0600

    Link style button

---
+++ frontend/src/components/Button.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  background: none;
+  border: none;
+  padding: 0;
+  font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+  color: #367ab5;
+  cursor: pointer;
+`;
+
+export default Button;
