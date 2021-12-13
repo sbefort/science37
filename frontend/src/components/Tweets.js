d16853dadc6579f2b15ccde1ae229aa048308128
Shaun Befort <shaun.befort@gomolearning.com>
2021-12-13 04:17:27 -0600

    Move tweets to separate component

---
+++ frontend/src/components/Tweets.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+import Tweet from './Tweet';
+import filterTweets from '../utils/filterTweets';
+
+const Tweets = ({ tweets, selectedHashtag, onHashtagClick }) => {
+  if (!tweets) {
+    return (
+      <P>Try searching to find some tweets!</P>
+    );
+  }
+
+  return (
+    <>
+      {filterTweets(tweets, selectedHashtag).map((tweet) => (
+        <Tweet key={tweet.id} tweet={tweet} selectedHashtag={selectedHashtag} onHashtagClick={onHashtagClick} />
+      ))}
+    </>
+  );
+};
+
+const P = styled.p`
+	text-align: center;
+  margin: 50px 0;
+  font-size: 1.5em;
+`;
+
+export default Tweets;
