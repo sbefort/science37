94d26c4049e6d97b7f0e2d7f0592c35c7f001ae5
Shaun Befort <shaun.befort@gomolearning.com>
2021-12-12 22:43:01 -0600

    Create useDebounce hook

---
+++ frontend/src/hooks/useDebounce.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react';
+
+// Instead of debouncing API requests, this hook debounces state changes
+// before the API request is made.
+const useDebounce = (value, delay) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+}
+
+export default useDebounce;
