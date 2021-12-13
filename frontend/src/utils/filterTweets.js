8691f6b0289b4459175d8ad70ce748b8cfddc006
Shaun Befort <shaun.befort@gomolearning.com>
2021-12-13 02:30:55 -0600

    Utility function for filtering tweets

---
+++ frontend/src/utils/filterTweets.js
@@ -0,0 +1,13 @@
+// Filters tweets by selected hashtag
+const filterTweets = (tweets, selectedHashtag) => {
+  if (!selectedHashtag) return tweets;
+
+  const filtered = tweets.filter((status) => {
+    const found = status.entities.hashtags.find((hashtag) => hashtag.text === selectedHashtag)
+    if (found) return true;
+    return false;
+  });
+  return filtered;
+}
+
+export default filterTweets;
