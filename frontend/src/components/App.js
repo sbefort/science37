dfd38c9d0594a0edc519667ecfe695d626e81fb1
Shaun Befort <shaun.befort@gomolearning.com>
2021-12-13 02:31:21 -0600

    Filter tweets by hashtag

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -9,9 +9,11 @@ import H2 from './H2';
 import TextInputWithIcon from './TextInputWithIcon';
 import Card from './Card';
 import Tweet from './Tweet';
+import Chip from './Chip';
 import Footer from './Footer';
 import useDebounce from '../hooks/useDebounce';
 import useTwitterProxy from '../hooks/useTwitterProxy';
+import filterTweets from '../utils/filterTweets';
 
 // If page is loaded with '?theme=dark' appended to URL, change to dark mode.
 const params = new URLSearchParams(window.location.search);
@@ -21,6 +23,8 @@ function App() {
   const [searchTerm, setSearchTerm] = useState('');
   const debouncedSearchTerm = useDebounce(searchTerm, 500);
   const [searchQuery, setSearchQuery] = useState('');
+  const [hashtags, setHashtags] = useState([]);
+  const [selectedHashtag, setSelectedHashtag] = useState('');
 
   // When the debounced search term changes, update the
   // query string to be sent to the backend proxy.
@@ -31,13 +35,34 @@ function App() {
   }, [debouncedSearchTerm]);
 
   const data = useTwitterProxy(searchQuery);
+  console.log(data);
+
+  useEffect(() => {
+    // Map through all search results and return all unique hashtags
+    const hashtags = data.statuses ? data.statuses.map((status) => status.entities.hashtags).flat()
+                                                  .map((hashtag) => hashtag.text)
+                                                  .filter((value, index, self) => self.indexOf(value) === index) : [];
+    setHashtags(hashtags);
+  }, [data]);
+
+  // Add or remove hashtags from the selectedHashtags filter when a hashtag is clicked
+  const onHashtagClick = (hashtag) => {
+    // If the hashtag is already selected, reset the filter
+    if (hashtag === selectedHashtag) {
+      setSelectedHashtag('');
+      return;
+    }
+
+    // If the hashtag was not already selected, and the hashtag was clicked, add it to the array
+    setSelectedHashtag(hashtag);
+  };
 
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
       <header>
         <H1>Tweet Feed</H1>
-        <MasonryItem width="70%">
+        <MasonryItem width="67%">
           <TextInputWithIcon
             onChange={(e) => setSearchTerm(e.target.value)}
             placeholder="Search by keyword"
@@ -46,15 +71,17 @@ function App() {
           />
         </MasonryItem>
       </header>
-      <MasonryItem width="27%" floatDirection="right">
+      <MasonryItem width="30%" floatDirection="right">
         <Card>
           <H2>Filter by hashtag</H2>
-          <p>Is my font size getting smaller?</p>
+          {hashtags && hashtags.map((hashtag) => (
+            <Chip onClick={() => onHashtagClick(hashtag)} key={hashtag}>#{ hashtag }</Chip>
+          ))}
         </Card>
       </MasonryItem>
-      <MasonryItem width="70%">
+      <MasonryItem width="67%">
         <Card>
-          {data.statuses && data.statuses.map((tweet) => (
+          {data.statuses && filterTweets(data.statuses, selectedHashtag).map((tweet) => (
             <Tweet key={tweet.id} tweet={tweet} />
           ))}
         </Card>
