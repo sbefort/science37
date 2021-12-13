27516e980b9d993baa61b01e796d8b7a989e5f88
Shaun Befort <shaun.befort@gomolearning.com>
2021-12-13 02:28:59 -0600

    Created Avatar component

---
+++ frontend/src/components/Avatar.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Avatar = ({ src, alt = "" }) => (
+  <StyledAvatar src={src} alt={alt} />
+);
+
+const StyledAvatar = styled.img`
+  vertical-align: middle;
+  width: 48px;
+  height: 48px;
+  border-radius: 50%;
+`;
+
+export default Avatar;
