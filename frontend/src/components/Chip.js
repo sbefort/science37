87e156a86abc7dddee0ccca7bd5b5f1a23fe8ad4
Shaun Befort <shaun.befort@gomolearning.com>
2021-12-13 02:29:11 -0600

    Created chip component

---
+++ frontend/src/components/Chip.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const Chip = styled.div`
+  background-color: #e7f3fa;
+  color: #367ab5;
+  border-radius: 1.5em;
+  display: inline-block;
+  padding: 0.8em 1.2em;
+  margin-right: 0.7em;
+  margin-bottom: 0.7em;
+  font-size: 0.8em;
+  cursor: pointer;
+`;
+
+export default Chip;
