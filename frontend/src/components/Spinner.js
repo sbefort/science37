64de277115a8be789f6968ac1a2a75fc2624a8e3
Shaun Befort <shaun.befort@gomolearning.com>
2021-12-13 23:16:11 -0600

    Spinner Component

---
+++ frontend/src/components/Spinner.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+import { CgSpinner } from "react-icons/cg";
+
+const Spinner = () => (
+  <StyledSpinner>
+    <CgSpinner className="spin" />
+  </StyledSpinner>
+  
+);
+
+const StyledSpinner = styled.div`
+  .spin {
+    animation: spin 0.7s infinite linear;
+    font-size: 30px;
+  }
+
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+
+    100% {
+      transform: rotate(359deg);
+    }
+  }
+`;
+
+export default Spinner;
