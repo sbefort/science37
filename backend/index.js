59971cc3c838c4b2beff5a9788ab5394c9a3c71a
Shaun Befort <shaun.befort@gomolearning.com>
2021-12-14 03:51:33 -0600

    Refactor of proxy

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
+const get = require('lodash/get');
+const TOKEN = 'Bearer AAAAAAAAAAAAAAAAAAAAAI4OHgEAAAAAlbk0HSIAqcc3havrrU9j2NeAQ34%3DzJmzwHuQerd8JJ2TeuHfqwKgBt6bK4tk93w3ocBB2vPuKMF3cG';
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -10,22 +12,31 @@ app.get('/api/v1/status', (req, res) => {
 });
 
 app.get('/api/v1/proxy', (req, res) => {
-  const query = [];
-  for (const q in req.query) {
-    if (req.query.hasOwnProperty(q)) {
-      query.push(`${q}=${encodeURIComponent(req.query[q])}`);
-    }
+  let qs;
+  // If the req.query.q key is set, get results for a new search term.
+  if (req.query.q) {
+    qs = `?q=${encodeURIComponent(req.query.q)}&result_type=popular&count=5`;
+  }
+
+  // If the req.query.next key is set, get the next results for a previous search term.
+  if (req.query.next) {
+    qs = req.query.next;
+  }
+
+  // If neither req.query.q or req.query.next is set, return an error
+  if (!qs) {
+    res.status(500).json('You must send one of the following query string params: q or next');
   }
 
   const headers = {
-    Authorization: 'Bearer AAAAAAAAAAAAAAAAAAAAAI4OHgEAAAAAlbk0HSIAqcc3havrrU9j2NeAQ34%3DzJmzwHuQerd8JJ2TeuHfqwKgBt6bK4tk93w3ocBB2vPuKMF3cG',
+    Authorization: TOKEN,
   };
 
-  axios.get(`https://api.twitter.com/1.1/search/tweets.json?${query.join('&')}`, {
+  axios.get(`https://api.twitter.com/1.1/search/tweets.json${qs}`, {
     headers,
   }).then((response) => {
     // If next_results exists in the response, query that endpoint to make sure it actually has more data.
-    const nextResults = response.data && response.data.search_metadata && response.data.search_metadata.next_results;
+    const nextResults = get(response, 'data.search_metadata.next_results');
     if (nextResults) {
       return Promise.all([
         Promise.resolve(response),
@@ -41,7 +52,8 @@ app.get('/api/v1/proxy', (req, res) => {
       ]
     }
   }).then(([currentResults, nextResults]) => {
-    const hasMoreResults = nextResults && nextResults.data && nextResults.data.statuses && nextResults.data.statuses.length > 0;
+    const hasMoreResults = get(nextResults, 'data.statuses', []).length > 0;
+    // Set a flag for whether or not more results are available for the same search term.
     if (hasMoreResults) {
       currentResults.data.search_metadata.has_more_results = true;
     } else {
@@ -49,7 +61,7 @@ app.get('/api/v1/proxy', (req, res) => {
     }
     res.json(currentResults.data);
   }).catch((err) => {
-    res.json(err)
+    res.status(500).json(err);
   });
 });
 
