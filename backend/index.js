e92b593ced7c5497a3f5d3e238b5048114ded6c0
Shaun Befort <shaun.befort@gomolearning.com>
2021-12-13 22:08:20 -0600

    Check for more results from Twitter

--- backend/index.js
+++ backend/index.js
@@ -17,17 +17,40 @@ app.get('/api/v1/proxy', (req, res) => {
     }
   }
 
+  const headers = {
+    Authorization: 'Bearer AAAAAAAAAAAAAAAAAAAAAI4OHgEAAAAAlbk0HSIAqcc3havrrU9j2NeAQ34%3DzJmzwHuQerd8JJ2TeuHfqwKgBt6bK4tk93w3ocBB2vPuKMF3cG',
+  };
+
   axios.get(`https://api.twitter.com/1.1/search/tweets.json?${query.join('&')}`, {
-    headers: {
-      Authorization: 'Bearer AAAAAAAAAAAAAAAAAAAAAI4OHgEAAAAAlbk0HSIAqcc3havrrU9j2NeAQ34%3DzJmzwHuQerd8JJ2TeuHfqwKgBt6bK4tk93w3ocBB2vPuKMF3cG',
+    headers,
+  }).then((response) => {
+    // If next_results exists in the response, query that endpoint to make sure it actually has more data.
+    const nextResults = response.data && response.data.search_metadata && response.data.search_metadata.next_results;
+    if (nextResults) {
+      return Promise.all([
+        Promise.resolve(response),
+        axios.get(`https://api.twitter.com/1.1/search/tweets.json${nextResults}`, {
+          headers,
+        })
+      ])
+    // Else next_results does not exist in the response and this is actually the last result set
+    } else {
+      return [
+        response,
+        undefined,
+      ]
     }
-  })
-    .then((response) => {
-      res.json(response.data);
-    })
-    .catch((err) => {
-      res.json(err)
-    });
+  }).then(([currentResults, nextResults]) => {
+    const hasMoreResults = nextResults && nextResults.data && nextResults.data.statuses && nextResults.data.statuses.length > 0;
+    if (hasMoreResults) {
+      currentResults.data.search_metadata.has_more_results = true;
+    } else {
+      currentResults.data.search_metadata.has_more_results = false;
+    }
+    res.json(currentResults.data);
+  }).catch((err) => {
+    res.json(err)
+  });
 });
 
 app.listen(3001, () =>
