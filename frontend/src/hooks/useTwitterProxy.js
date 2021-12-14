641da231ffc9b3a7b2b81c231794de8f101ea472
Shaun Befort <shaun.befort@gomolearning.com>
2021-12-13 22:10:32 -0600

    Refactor for loading more results

--- frontend/src/hooks/useTwitterProxy.js
+++ frontend/src/hooks/useTwitterProxy.js
@@ -6,15 +6,24 @@ const useTwitterProxy = (searchQuery) => {
 
   useEffect(() => {
     // If the search term is blank or invalid, skip the API request and return an empty object
-    if (!searchQuery || searchQuery.indexOf('q=&') === 0 || searchQuery.indexOf('q=%23&') === 0) {
+    if (!searchQuery || searchQuery.indexOf('?q=&') === 0 || searchQuery.indexOf('?q=%23&') === 0) {
       setData({});
       return;
     }
 
+    // When NOT loading next page of results for the same search term, reset the data object
+    if (!searchQuery.indexOf('?max_id=') === 0) {
+      setData({});
+    }
+
     (async () => {
-      const response = await axios.get(`/api/v1/proxy?${searchQuery}`);
-      if (response) {
-        setData(response.data);
+      const response = await axios.get(`/api/v1/proxy${searchQuery}`);
+      if (response.data) {
+        const { search_metadata, statuses } = response.data;
+        setData((currentState) => ({
+          search_metadata,
+          statuses: currentState.statuses ? currentState.statuses.concat(statuses) : statuses
+        }));
       }
     })();
   }, [searchQuery]);
