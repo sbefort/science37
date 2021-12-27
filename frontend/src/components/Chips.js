5a345c32c144a825c448ec95d33099ac2b385e92
Shaun Befort <shaun.befort@gomolearning.com>
2021-12-27 10:37:00 -0600

    Added container for Chip components

---
+++ frontend/src/components/Chips.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+// Why negative margin?
+//
+// Each chip has a left and right margin of 0.4em.
+// When the chips wrap, the parent container needs negative margin for wrapped chips to line up with the other content.
+const Chips = styled.div`
+  margin-left: -0.4em;
+  margin-right: -0.4em;
+`;
+
+export default Chips;
