bc24939a0d15aa6e1394e3201941358af1eebd5f
Shaun Befort <shaun.befort@gomolearning.com>
2021-12-27 10:31:49 -0600

    Refactor backend code

--- backend/index.js
+++ backend/index.js
@@ -1,70 +1,90 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const axios = require('axios');
-const get = require('lodash/get');
 const TOKEN = 'Bearer AAAAAAAAAAAAAAAAAAAAAI4OHgEAAAAAlbk0HSIAqcc3havrrU9j2NeAQ34%3DzJmzwHuQerd8JJ2TeuHfqwKgBt6bK4tk93w3ocBB2vPuKMF3cG';
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+
+const headers = {
+  Authorization: TOKEN,
+};
 
 app.get('/api/v1/status', (req, res) => {
   res.json({ status: 'OK', version: 'v1' });
 });
 
-app.get('/api/v1/proxy', (req, res) => {
+app.get('/api/v1/proxy', async (req, res) => {
   let qs;
-  // If the req.query.q key is set, get results for a new search term.
-  if (req.query.q) {
-    qs = `?q=${encodeURIComponent(req.query.q)}&result_type=popular&count=5`;
-  }
 
-  // If the req.query.next key is set, get the next results for a previous search term.
-  if (req.query.next) {
-    qs = req.query.next;
-  }
+  try {
+    // If the req.query.q key is set, get results for a new search term.
+    if (req.query.q) {
+      qs = `?q=${encodeURIComponent(req.query.q)}&result_type=popular&count=5`;
+    }
 
-  // If neither req.query.q or req.query.next is set, return an error
-  if (!qs) {
-    res.status(500).json('You must send one of the following query string params: q or next');
-  }
+    // If the req.query.next key is set, get the next results for the current search term.
+    if (req.query.next) {
+      qs = req.query.next;
+    }
 
-  const headers = {
-    Authorization: TOKEN,
-  };
+    // If neither req.query.q or req.query.next is set, return an error
+    if (!qs) {
+      res.status(500).json('You must send one of the following query string params: q or next');
+    }
 
-  axios.get(`https://api.twitter.com/1.1/search/tweets.json${qs}`, {
-    headers,
-  }).then((response) => {
+    const response = await axios.get(`https://api.twitter.com/1.1/search/tweets.json${qs}`, {
+      headers,
+    });
+
+    const data = massageTwitterResponse(response);
+
     // If next_results exists in the response, query that endpoint to make sure it actually has more data.
-    const nextResults = get(response, 'data.search_metadata.next_results');
-    if (nextResults) {
-      return Promise.all([
-        Promise.resolve(response),
-        axios.get(`https://api.twitter.com/1.1/search/tweets.json${nextResults}`, {
-          headers,
-        })
-      ])
-    // Else next_results does not exist in the response and this is actually the last result set
-    } else {
-      return [
-        response,
-        undefined,
-      ]
-    }
-  }).then(([currentResults, nextResults]) => {
-    const hasMoreResults = get(nextResults, 'data.statuses', []).length > 0;
-    // Set a flag for whether or not more results are available for the same search term.
-    if (hasMoreResults) {
-      currentResults.data.search_metadata.has_more_results = true;
-    } else {
-      currentResults.data.search_metadata.has_more_results = false;
-    }
-    res.json(currentResults.data);
-  }).catch((err) => {
+    data.nextResults = await nextResults(response.data.search_metadata.next_results);
+
+    res.json(data);
+  } catch (err) {
     res.status(500).json(err);
-  });
+  }
 });
 
 app.listen(3001, () =>
   console.log('Express server is running on localhost:3001')
 );
+
+const massageTwitterResponse = (response) => {
+  const data = {};
+  data.tweets = response.data.statuses.map((status) => {
+    const { id_str: id, text, user } = status;
+    const hashtags = status.entities.hashtags.map((hashtag) => hashtag.text);
+
+    return ({
+      id,
+      text,
+      hashtags,
+      user: {
+        username: user.screen_name,
+        avatarUrl: user.profile_image_url_https,
+      }
+    });
+  });
+
+  return data;
+};
+
+// Takes a search_metadata.next_results pagination query string like
+// ?max_id=1474698938800386049&q=cats&count=5&include_entities=1&result_type=popular
+// to make sure that the query actually returns more results.
+// If the query string fetches more results, return the query string to be used on the frontend.
+// If the query string does not fetch more results, return undefined so it is not used on the frontend.
+const nextResults = async (nextResults) => {
+  if (!nextResults) return false;
+  const response = await axios.get(`https://api.twitter.com/1.1/search/tweets.json${nextResults}`, {
+    headers,
+  });
+
+  if (Array.isArray(response.data.statuses) && response.data.statuses.length > 0) {
+    return nextResults;
+  }
+
+  return undefined;
+};
